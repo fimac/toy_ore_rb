12cbf34bf5ac4491d6221897bf72748ed33a7a7a
fimac <fiona@cipherstash.com>
2023-02-02 17:44:16 +1100

    update to dot notation

--- spec/scheme_spec.rb
+++ spec/scheme_spec.rb
@@ -1,21 +1,21 @@
-RSpec.describe ToyOre::OreScheme do
+RSpec.describe ToyOre::Scheme::OreScheme do
   describe "OreScheme initialization" do
     it "left ciphertexts are deterministic" do
-      ore = ToyOre::OreScheme.new()
+      ore =ToyOre::Scheme::OreScheme.new()
       ct_one = ore.encrypt(42)
 
       ct_two = ore.encrypt(42)
 
-      expect(ct_one[:left][:key]).to eq(ct_two[:left][:key])
+      expect(ct_one.left.key).to eq(ct_two.left.key)
     end
 
     it "right ciphertexts are non deterministic" do
-      ore = ToyOre::OreScheme.new()
+      ore =ToyOre::Scheme::OreScheme.new()
 
       ct_one = ore.encrypt(42)
       ct_two = ore.encrypt(42)
 
-      expect(ct_one[:right][:encryptions]).not_to eq(ct_two[:right][:encryptions])
+      expect(ct_one.right.encryptions).not_to eq(ct_two.right.encryptions)
     end
   end
 end
@@ -40,29 +40,29 @@ RSpec.describe ToyOre::Scheme do
 
   describe "compare_ciphertexts" do
     it "returns -1 if left_ct < right_ct" do
-      ore_client = ToyOre::OreScheme.new()
+      ore_client =ToyOre::Scheme::OreScheme.new()
       ct_one = ore_client.encrypt(42)
       ct_two = ore_client.encrypt(43)
 
-      result = ToyOre::Scheme.compare_ciphertexts(ct_one[:left], ct_two[:right])
+      result = ToyOre::Scheme.compare_ciphertexts(ct_one.left, ct_two.right)
       expect(result).to eq(-1)
     end
 
     it "returns 0 if left_ct == right_ct" do
-      ore_client = ToyOre::OreScheme.new()
+      ore_client =ToyOre::Scheme::OreScheme.new()
       ct_one = ore_client.encrypt(42)
       ct_two = ore_client.encrypt(42)
 
-      result = ToyOre::Scheme.compare_ciphertexts(ct_one[:left], ct_two[:right])
+      result = ToyOre::Scheme.compare_ciphertexts(ct_one.left, ct_two.right)
       expect(result).to eq(0)
     end
 
     it "returns 1 if left_ct > right_ct" do
-      ore_client = ToyOre::OreScheme.new()
+      ore_client =ToyOre::Scheme::OreScheme.new()
       ct_one = ore_client.encrypt(43)
       ct_two = ore_client.encrypt(42)
 
-      result = ToyOre::Scheme.compare_ciphertexts(ct_one[:left], ct_two[:right])
+      result = ToyOre::Scheme.compare_ciphertexts(ct_one.left, ct_two.right)
       expect(result).to eq(1)
     end
   end
